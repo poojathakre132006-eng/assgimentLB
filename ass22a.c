8164a230053bfe1c260ad69121e44c61ffc95c87
pooja <poojathakre132006@gmail.com>
2025-11-15 19:01:09 +0530

    Add files via upload

---
+++ ass22a.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+typedef int*IPTR;
+
+int FrequencyCalculate(int Arr[],int iSize,int iNo)
+{
+  int iCnt=0, iCount=0;
+
+  for(iCnt=0;iCnt<=iSize;iCnt++)
+  {
+   if(Arr[iCnt]==iNo)
+   {
+    iCount++;
+   }
+  }
+return iCount;
+  
+}
+
+
+int main()
+{
+    int iLength=0,iCnt=0,iRet=0;
+    IPTR iPtr=NULL;
+
+    printf("enter the numbere of elements:");
+    scanf("%d",&iLength);
+    
+    //step 1:Allocate the memory
+    iPtr=(IPTR)malloc(iLength*sizeof(int));
+
+    if(NULL==iPtr)
+    {
+        printf("Unable to allocate the memory\n");
+        return -1;
+    }
+
+    printf("enter the values:\n");
+
+    for(iCnt=0;iCnt<iLength;iCnt++)
+    {
+        sacnf("%d",&iPtr[iCnt]);
+    }
+
+    //step 2:use the memory
+    iRet=FrequencyCalculate(iPtr,iLength);
+    printf("Frequecy of iNo is :%d\n",iRet);
+
+    //step 3:free the memory
+    free(iPtr);
+    return 0;
+}
